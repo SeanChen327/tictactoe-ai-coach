094a2e17c2535a9e844321e4db2d9a81f39b922f
SeanChen327 <yongxic1@bu.edu>
2026-03-30 15:34:08 -0400

    Change GEMINI_API_KEY in setup_db.py
    Updated GEMINI_API_KEY for API configuration.

--- setup_db.py
+++ setup_db.py
@@ -3,7 +3,7 @@ from pinecone import Pinecone
 from google.genai.types import EmbedContentConfig
 
 # 1. API Configuration
-GEMINI_API_KEY = "AIzaSyCvpS6f_UrgAS2FhdD6ZEK32WrBbsxRONo" 
+GEMINI_API_KEY = "AIzaSyD8D29a60Uvu7FOR5AbJaA3ZQ0vS2zbcV4" 
 PINECONE_API_KEY = "pcsk_2tCAii_HNhEUG8tnrrw6bfrNxVFYa7A4eBVdjjZESMFyMwuRQ5o1vZXn1KsoGtuA3f6GEq"
 
 # Initialize clients
